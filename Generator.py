81d35c6288cc28909b739a3a9061a94bdfc49247
Victor-Wouters <victorwouters@hotmail.com>
2024-03-04 20:04:44 +0100

    Adjusted days

--- Generator.py
+++ Generator.py
@@ -8,7 +8,7 @@ import random
 if __name__ == '__main__':
 
     #Initializations
-    days_list = ["2024-03-01"] 
+    days_list = ["2024-03-01","2024-03-02"] 
     amount_transactions = 200 # Amount of DVP transactions per day, x2 transactions/day
     amount_participants = 25
     amount_securities = 3
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     #Generate transactions
     transaction_df = TransactionData.generate_transaction_data(amount_transactions, amount_participants, amount_securities, days_list, min_transaction_value,max_transaction_value)
-
+    print(transaction_df)
     #Generate participants
 
     balance_df = ParticipantData.generate_transaction_data(amount_participants, amount_securities, min_balance_value, max_balance_value)
