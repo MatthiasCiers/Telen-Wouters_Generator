b6a20118fd7e758ac65272be5e1faa2e6e406a56
Victor-Wouters <victorwouters@hotmail.com>
2024-03-21 13:35:33 +0100

    Changes in output format

--- ParticipantData.py
+++ ParticipantData.py
@@ -20,18 +20,25 @@ def generate_participant_data_modified(amount_participants, amount_securities, m
     balance_df = pd.DataFrame(columns=[ 'Part ID','Account ID', 'Balance', 'Credit limit'])
 
     for participant in range(1,amount_participants+1):
-        total_balance_part = 0
-        for account_type in range(amount_securities,-1,-1):
+        total_securities_part = 0
+        for account_type in range(amount_securities+1):
             if account_type == 0:
-                balance_value = round(random.uniform(0.02, 0.1) * total_balance_part, 2)
-                credit_limit =  round(random.uniform(0.25, 1) * balance_value, 2)
+                balance_value = None
+                credit_limit =  None
                 new_row_balance = pd.DataFrame({ 'Part ID': participant,'Account ID': account_type, 'Balance': balance_value, 'Credit limit': credit_limit}, index=[0])
                 balance_df = pd.concat([balance_df, new_row_balance], ignore_index=True)
             else: 
                 balance_value = random.randint(min_balance_value,max_balance_value)
-                total_balance_part += balance_value
+                total_securities_part += balance_value
                 credit_limit =  0
                 new_row_balance = pd.DataFrame({ 'Part ID': participant,'Account ID': account_type, 'Balance': balance_value, 'Credit limit': credit_limit}, index=[0])
                 balance_df = pd.concat([balance_df, new_row_balance], ignore_index=True)
+
+        part_condition = (balance_df['Part ID'] == participant)
+        acc_condition = (balance_df['Account ID'] == 0)
+        balance_value = round(random.uniform(0.02, 0.1) * total_securities_part, 2)
+        balance_df.loc[part_condition & acc_condition, 'Balance'] = balance_value
+        credit_limit =  round(random.uniform(0.25, 1) * balance_value, 2)
+        balance_df.loc[part_condition & acc_condition, 'Credit limit'] = credit_limit
     
     return balance_df
